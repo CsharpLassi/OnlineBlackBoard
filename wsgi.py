e546bff2e9f3c88e39b3a00e6a71a36296bd4ef2
Lassi <csharplassi@posteo.de>
2020-10-14 19:10:55 +0200

    Add: Proxy Fix

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
 from onlineblackboard import create_app
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = create_app()
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
