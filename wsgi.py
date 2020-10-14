a960e382844f434b4858bb8207eddc626ba49a57
Lassi <csharplassi@posteo.de>
2020-10-14 19:23:30 +0200

    Fix: gunicorn

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,5 @@
 from onlineblackboard import create_app
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-from OnlineBlackBoard.onlineblackboard.ext import socket
-
 app = create_app()
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
