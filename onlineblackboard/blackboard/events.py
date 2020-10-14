4aae1c5e746ce679581269a52582012c50745e63
Lassi <csharplassi@posteo.de>
2020-10-14 17:46:22 +0200

    Create Basic Markdown

--- onlineblackboard/blackboard/events.py
+++ onlineblackboard/blackboard/events.py
@@ -2,9 +2,9 @@ from flask_socketio import emit
 
 from ..ext import socket
 
-namespace = 'blackboard'
+namespace = '/blackboard'
 
 
-@socket.on('connect', namespace=namespace)
-def on_connect():
-    pass
+@socket.on('connect', namespace='/blackboard')
+def blackboard_connect():
+    emit('print_content', {'markdown': '# Connected'})
