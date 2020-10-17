cb38ddb574859b6a4601c6618f2028ae279ec88e
Lassi <csharplassi@posteo.de>
2020-10-17 08:52:21 +0200

    Update Bug #4: Add Length-Validator

--- onlineblackboard/blackboard/forms.py
+++ onlineblackboard/blackboard/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class CreateSessionForm(FlaskForm):
-    room_id = StringField('ID', validators=[DataRequired()])
+    room_id = StringField('ID', validators=[DataRequired(), Length(min=6, max=10)])
     submit = SubmitField('Create Session')
 
 
 class ConnectForm(FlaskForm):
-    room_id = StringField('ID', validators=[DataRequired()])
+    room_id = StringField('ID', validators=[DataRequired(), Length(min=6, max=10)])
     submit = SubmitField('Connect')
