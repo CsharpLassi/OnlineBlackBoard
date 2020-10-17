fad6bba2b45b1b3f2f427a68b2cd806911e230a8
Lassi <csharplassi@posteo.de>
2020-10-17 14:51:58 +0200

    Add Function #7: Edit User and Change Password

--- onlineblackboard/users/forms_admin.py
+++ onlineblackboard/users/forms_admin.py
@@ -1,10 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class EditForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
 
-    submit = SubmitField('Register')
+    submit = SubmitField('Edit')
+
+
+class ChangePasswordForm(FlaskForm):
+    password = PasswordField('New Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat New Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Change Password')
--- onlineblackboard/users/views_admin.py
+++ onlineblackboard/users/views_admin.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template, flash, redirect, url_for
+from flask import Blueprint, render_template, flash, redirect, url_for, request
+
+from ..tools import admin_required
+from ..ext import db
 
 from .models import User
-from ..tools import admin_required
 
 bp = Blueprint('users.admin', __name__, url_prefix='/users/admin')
 
@@ -16,15 +18,50 @@ def home():
 @bp.route('/<int:user_id>', methods=['GET', "POST"])
 @admin_required
 def detail(user_id: int):
-    from .forms_admin import EditForm
+    from .forms_admin import EditForm, ChangePasswordForm
 
     user = User.query.get(user_id)
     if not user:
         flash('User not found')
         return redirect(url_for('users.admin.home'))
 
-    edit_form = EditForm()
-    if edit_form.validate_on_submit():
-        pass
+    update_counter = 0
 
-    return render_template('users/admin/detail.html', user=user, edit_form=edit_form)
+    edit_form = EditForm()
+    if request.args.get('edit') == 'user' and edit_form.validate_on_submit():
+        new_email = edit_form.email.data
+        new_username = edit_form.username.data
+
+        if new_email != user.email:
+            if User.query.filter_by(email=new_email).count() == 0:
+                user.email = new_email
+                update_counter += 1
+            else:
+                flash('E-Mail already exist')
+
+        if new_username != user.username:
+            if User.query.filter_by(username=new_username).count() == 0:
+                user.username = new_username
+                update_counter += 1
+            else:
+                flash('Username already exist')
+
+    change_password_form = ChangePasswordForm()
+    if request.args.get('edit') == 'password' and \
+            change_password_form.validate_on_submit():
+        new_password = change_password_form.password.data
+        user.set_password(new_password)
+        update_counter += 1
+
+    change_password_form.password.data = None
+    change_password_form.password2.data = None
+
+    if update_counter > 0:
+        db.session.commit()
+
+    edit_form.username.data = user.username
+    edit_form.email.data = user.email
+
+    return render_template('users/admin/detail.html', user=user,
+                           edit_form=edit_form,
+                           change_password_form=change_password_form)
