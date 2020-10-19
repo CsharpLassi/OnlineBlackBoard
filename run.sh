f4f55ccdf9e6efbe794876aa1a0bf2f0bc5595c1
Lassi <csharplassi@posteo.de>
2020-10-19 21:03:37 +0200

    Fix: Dockerfile

--- run.sh
+++ run.sh
@@ -8,5 +8,4 @@ flask db migrate
 flask db upgrade
 
 #Start Service
-cd src || exit
-gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 --bind $HOST:5000 wsgi:app
+gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 --bind $HOST:5000
