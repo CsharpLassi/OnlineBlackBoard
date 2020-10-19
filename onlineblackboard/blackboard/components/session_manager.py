d94f16f064513b7b94207be168e5feeddfe82465
Lassi <csharplassi@posteo.de>
2020-10-19 16:41:25 +0200

    Create BlackboardManager

---
+++ onlineblackboard/blackboard/components/session_manager.py
@@ -0,0 +1,2 @@
+class BlackBoardSessionHandler:
+    pass
--- onlineblackboard/blackboard/ext.py
+++ onlineblackboard/blackboard/ext.py
@@ -1,17 +1,13 @@
 import flask
 from werkzeug.local import LocalProxy
 
+from .components.session_manager import BlackBoardSessionHandler
 from .server_models import BlackboardRoomSession, UserSessions
 from ..tools.MemDb import MemDb
 
 namespace = '/blackboard'
+
+bb_manager = BlackBoardSessionHandler()
+
 room_db = MemDb[int, BlackboardRoomSession]()
-
 user_db = MemDb[str, UserSessions]()
-
-
-def check_rooms():
-    room: BlackboardRoomSession
-    for room_id, room in room_db.items():
-        if len(room.users) == 0:
-            room_db.pop(room_id)
