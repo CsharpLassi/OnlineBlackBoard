8a5cbccee40efe9b4d3bf3b9fc0b88b0e15f249e
Lassi <csharplassi@posteo.de>
2020-10-20 09:28:36 +0200

    add: create_user to cli

--- src/obb/users/cli.py
+++ src/obb/users/cli.py
@@ -6,6 +6,20 @@ from ..ext import db
 bp_cli = Blueprint("user", __name__)
 
 
+@bp_cli.cli.command('create')
+@click.argument('name')
+@click.argument('email')
+@click.argument('password')
+def create_user(name, email, password):
+    from .models import User
+    admin = User(username=name,
+                 email=email)
+    admin.set_password(password)
+
+    db.session.add(admin)
+    db.session.commit()
+
+
 @bp_cli.cli.command('create_admin')
 @click.argument('name')
 @click.argument('email')
