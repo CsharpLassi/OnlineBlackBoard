3481c809e8a8d0156dab8fa234a980c56cb6efe9
Lassi <csharplassi@posteo.de>
2020-10-26 19:24:43 +0100

    Remove old Session Handling

--- src/obb/blackboard/ext.py
+++ src/obb/blackboard/ext.py
@@ -1,7 +1,2 @@
-from .components.page_manager import PageManager
-from .components.session_manager import BlackBoardSessionManager
-
 namespace = '/blackboard'
 
-bb_session_manager = BlackBoardSessionManager()
-page_manager = PageManager()
