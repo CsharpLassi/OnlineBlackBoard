e0432bad9895e5d6e3cefe1d34dba4cdfbf39568
Lassi <csharplassi@posteo.de>
2020-10-30 13:14:39 +0100

    Add Length in session names

--- src/obb/blackboard/forms/session.py
+++ src/obb/blackboard/forms/session.py
@@ -1,14 +1,14 @@
 import datetime
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, DateTimeField, IntegerField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, DateTimeField, IntegerField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 from obb.tools.forms import BaseDataForm
 
 
 class CreateSessionForm(FlaskForm, BaseDataForm):
-    name = StringField("Name", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired(), Length(max=128)])
 
     room_name = StringField("Room", validators=[DataRequired()])
 
