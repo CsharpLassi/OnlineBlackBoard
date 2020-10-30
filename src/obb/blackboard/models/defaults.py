97ae1823170ab3e8dd326bb7aa260f46eb57ac85
Lassi <csharplassi@posteo.de>
2020-10-30 15:12:07 +0100

    Add inline test for default values

--- src/obb/blackboard/models/defaults.py
+++ src/obb/blackboard/models/defaults.py
@@ -4,5 +4,12 @@ visibilities = ("creator_only", "public")
 default_draw_height = 256
 default_draw_width = 1024
 
+assert default_draw_width > 1, "default_draw_height must be greater than 1"
+assert default_draw_width > 1, "default_draw_width must be greater than 1"
+
 # Blackboard
 default_blackboard_visibility = "creator_only"
+
+assert (
+    default_blackboard_visibility in visibilities
+), "default_blackboard_visibility mus be in visibilities"
